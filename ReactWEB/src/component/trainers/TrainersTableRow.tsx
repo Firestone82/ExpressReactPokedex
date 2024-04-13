83abcbc8aafdaae02f33cc10f6b6e36b275fce9e
Orfian <Orfian963@gmail.com>
2024-04-13 20:14:40 +0200

    surpris

--- ReactWEB/src/component/trainers/TrainersTableRow.tsx
+++ ReactWEB/src/component/trainers/TrainersTableRow.tsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import TableCell from "@mui/material/TableCell";
 import TableRow from "@mui/material/TableRow";
 import { Link } from "@mui/material";
+import { api } from "../../types/app";
 
 export default function TrainersTableRow({
   row,
@@ -10,6 +11,33 @@ export default function TrainersTableRow({
   row: any;
   onPokemonInfo: (id: number) => void;
 }) {
+  const [sprites, setSprites] = useState<string[]>([]);
+  const [pokemonNames, setPokemonNames] = useState<string[]>([]);
+
+  useEffect(() => {
+    const fetchPokemonData = async () => {
+      const namePromises = row.pokemon.map(async (pok: any) => {
+        const response = await fetch(`${api}/pokemon/${pok}`);
+        const data = await response.json();
+        return data.name;
+      });
+  
+      const names = await Promise.all(namePromises);
+      setPokemonNames(names);
+  
+      const spritePromises = names.map(async (name: string) => {
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name.toLowerCase()}`);
+        const data = await response.json();
+        return data.sprites.front_default;
+      });
+  
+      const spriteUrls = await Promise.all(spritePromises);
+      setSprites(spriteUrls);
+    };
+  
+    fetchPokemonData();
+  }, [row.pokemon]);  
+
   return (
     <TableRow key={row.id}>
       <TableCell component="th" scope="row">
@@ -19,12 +47,11 @@ export default function TrainersTableRow({
       <TableCell>{row.email}</TableCell>
       <TableCell align="center" style={{ paddingRight: '8px' }}>
         {row.pokemon.map((pok: any, pokIndex: number) => (
-          <React.Fragment key={pok}>
-            <Link onClick={() => onPokemonInfo(pok)} sx={{ cursor: "pointer" }}>
-              {pok}
-            </Link>
-            {pokIndex < row.pokemon.length - 1 ? ", " : ""}
-          </React.Fragment>
+          <Link onClick={() => onPokemonInfo(pok)} sx={{ cursor: "pointer" }}>
+            {sprites[pokIndex] && (
+              <img src={sprites[pokIndex]} alt={`Sprite for ${pok}`} />
+            )}
+          </Link>
         ))}
       </TableCell>
     </TableRow>
