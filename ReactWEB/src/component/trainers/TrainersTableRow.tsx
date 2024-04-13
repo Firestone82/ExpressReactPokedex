474e31daec37b79b6264371289d73d152dba5225
Orfian <Orfian963@gmail.com>
2024-04-13 19:16:54 +0200

    Pokemon info

--- ReactWEB/src/component/trainers/TrainersTableRow.tsx
+++ ReactWEB/src/component/trainers/TrainersTableRow.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import TableCell from "@mui/material/TableCell";
 import TableRow from "@mui/material/TableRow";
+import { Link, Stack } from "@mui/material";
+import { api } from "../../types/app";
+import PokemonInfo from "../pokemon/PokemonInfo";
 
 export default function TrainersTableRow({
   row,
+  onPokemonInfo,
 }: {
   row: any;
+  onPokemonInfo: (id: number) => void;
 }) {
   return (
     <TableRow key={row.id}>
@@ -14,7 +19,16 @@ export default function TrainersTableRow({
       </TableCell>
       <TableCell>{row.name}</TableCell>
       <TableCell>{row.email}</TableCell>
-      <TableCell align="center">{row.pokemon}</TableCell>
+      <TableCell align="center" style={{ paddingRight: '8px' }}>
+        {row.pokemon.map((pok: any, pokIndex: number) => (
+          <React.Fragment key={pok}>
+            <Link onClick={() => onPokemonInfo(pok)}>
+              {pok}
+            </Link>
+            {pokIndex < row.pokemon.length - 1 ? ", " : ""}
+          </React.Fragment>
+        ))}
+      </TableCell>
     </TableRow>
   );
 }
